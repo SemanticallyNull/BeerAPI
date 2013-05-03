7ab4641e93ed75f5a53f918ae5539b56e8882b95
Ben Chapman <ben@dismiz.com>
2013-05-03 16:10:51 +0100

    beerinfo

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -1,7 +1,7 @@
 angular.module('beercheck', []).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.
-      when('/', {templateUrl: 'partials/home.html', controller: BeerListCtrl}).
+      when('/', {templateUrl: 'partials/home.html', controller: BeerCtrl}).
       when('/beer/:beerId', {templateUrl: 'partials/beer.html', controller: BeerCtrl}).
       when('/log', {templateUrl: 'partials/take.html', controller: BeerTakeCtrl}).
       otherwise({redirectTo: '/'});
--- public/js/index.js
+++ public/js/index.js
@@ -3,17 +3,16 @@ function BeerListCtrl($scope,$http) {
     $scope.beers = data;
   });
 };
-function BeerCtrl($scope,$http,$routeParams) {
-  $scope.beerId = $routeParams.beerId
-  $http.get('/beer/'+$routeParams.beerId+'?api_key='+apikey).success(function(data){
-    $scope.beer = data;
-  });
+function BeerCtrl($scope,$http) {
+  $scope.info = function() {
+    if(this.beercode) {
+      $http.get('/beer/'+this.beercode+'?api_key='+apikey).success(function(data){
+        $scope.beer = data;
+      });
+    }
+  }
 };
 function BeerTakeCtrl($scope,$http,$routeParams) {
-  var beerdata;
-  $http.get('/beers?api_key='+apikey).success(function(data){
-    beerdata = data;
-  });
   $scope.take = function() {
     if(this.beercode) {
       $http.get('/beer/'+this.beercode+'/take?api_key='+apikey).success(function(data){
